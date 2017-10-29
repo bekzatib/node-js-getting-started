91ae87cfe937aaf271aa982cd73d4c83e7f71003
Bekzat <bekzat.dev@gmail.com>
2017-10-28 18:16:27 -0700

    cool path added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var cool = require('cool-ascii-faces');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -13,6 +13,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/cool', function(request, response){
+  response.send(cool());
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
